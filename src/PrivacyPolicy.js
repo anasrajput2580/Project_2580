49a38d4acba6365ae853e6c57e549b5d4dd5a118
Anas <anasrajput2580@gmail.com>
2025-01-28 22:00:47 +0500

    Update Privacy Policy

--- src/PrivacyPolicy.js
+++ src/PrivacyPolicy.js
@@ -6,101 +6,107 @@ const PrivacyPolicy = () => {
     <div className="Bg-control-privacy">
     <div className="privacy-policy-container">
     <p className="privacy-paragraph">
-      Treasure Hint ("we", "our", "us") is committed to protecting your privacy. This Privacy Policy explains how we ​collect, use, disclose, and safeguard your information when you visit our website, use our services, or participate ​in the Treasure Hint Affiliate Program. Please read this policy carefully to understand our views and practices ​regarding your personal data and how we will treat it.
+    Treasure Hint ("we," "our," "us") is committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website, use our services, or participate in the Treasure Hint Affiliate Program. It also describes our compliance with SMS/MMS regulations, including opt-in consent, opt-out procedures, and data retention practices. Please read this policy carefully to understand how we handle your data.
     </p>
-    <h2 className="privacy-subheader">Information We Collect</h2>
-    <p className="privacy-paragraph">
+    <h2 className="privacy-subheader">Scope of This Policy</h2>
+    <p className="privacy-paragraph"> This Privacy Policy applies to all data collected through our website, mobile applications, SMS/MMS campaigns, and other services Treasure Hint offers.</p>
+    <h2 className="privacy-header">Information We Collect</h2>
+    <h2 className="privacy-subheader">Personal Data</h2>
+    <p className="privacy-paragraph">We may collect personal identification information, including:
         <ul>
-            <li> <b>Personal Data:</b> We may collect personal identification information such as your name, email address, phone ​number, mailing address, payment information, and other details necessary to provide our services.
+            <li> Name, email address, phone number, and mailing address.
             </li>
-            <li> <b>Non-Personal Data:</b> When you interact with our site, we may collect non-personal identification information ​about you. This may include the browser name, the type of computer, and technical information about your ​means of connection to our site, such as the operating system, used Internet service providers, and other ​similar information.
+            <li> Payment information for processing transactions.
             </li>
-            <li>  <b>Usage Data:</b> We may collect information on how you access and use the website, including your IP address, ​browser type, pages visited, time spent on pages, and other similar data.  
+            <li>  Data necessary to provide services, including preferences and interests.
             </li>
          </ul>
     </p>
+    <h2 className="privacy-subheader"> Non-Personal Data</h2>
+    <p className="privacy-paragraph"> We collect non-personal identification information such as browser type, device type, operating system, and usage details for analytics and site optimization.
+    </p>
+    <h2 className="privacy-subheader"> Usage Data</h2>
+    <p className="privacy-paragraph"> We may collect information such as IP address, browser type, pages visited, time spent on pages, and other usage data to improve the user experience and enhance security.</p>
     <h2 className="privacy-subheader">How We Use Your Information</h2>
-    <p className="privacy-paragraph">We may use the information we collect from you in the following ways:</p>
+    
+    <p className="privacy-paragraph">We use the data collected for the following purposes</p>
     <p className="privacy-paragraph">
     <ul>
-        <li><strong>To Personalize Your Experience:</strong> Your information helps us better respond to your individual needs.</li>
-        <li><strong>To Improve Our Website:</strong> We continually strive to improve our website offerings based on the information and feedback we receive from you.</li>
-        <li><strong>To Improve Customer Service:</strong> Your information helps us to more effectively respond to your customer service requests and support needs.</li>
-        <li><strong>To Process Transactions:</strong> We may use the information you provide about yourself when placing an order only to provide service to that order. We do not share this information with outside parties, except to the ​extent necessary to provide the service.</li>
-        <li><strong>To administer a contest, promotion, survey, or any other feature on our site:</strong> We will send you information about topics we believe you'll find interesting.</li>
-        <li><strong>To Send Periodic Emails:</strong> The email address you provide may be used to send you information, respond to ​inquiries, and/or answer other requests or questions.</li>
-      </ul>
-    </p>
-    <h2 className="privacy-subheader">How We Protect Your Information</h2>
-      <p className="privacy-paragraph">We adopt appropriate data collection, storage, and processing practices as well as security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, ​password, transaction information, and data stored on our site.</p>
-      <h2 className="privacy-subheader">Sharing Your Personal Information</h2>
-      <p className="privacy-paragraph">We do not sell, trade, or rent users' personal information to others. We may share generic aggregated ​demographic information not linked to any personal identification information with our business partners, trusted affiliates, and advertisers. We may also use third-party service providers to help us operate our business ​and the website or administer activities on our behalf, such as sending out newsletters or surveys.</p>
-      <h2 className="privacy-subheader">Cookies and Tracking Technologies</h2>
-      <p className="privacy-paragraph">We use cookies and other tracking technologies to enhance your experience on our website, analyze site usage, ​​and assist in our marketing efforts. Cookies help us remember your preferences and actions over time. We use essential cookies for website functionality, performance cookies to analyze site usage, functional cookies to remember your choices, and advertising cookies to deliver relevant ads. You can manage cookies through your browser settings or opt out of certain advertising cookies via industry opt-out pages.
-      </p>
-      <h2 className="privacy-subheader">Your Data Protection Rights</h2>
-    <p className="privacy-paragraph">You have the following rights regarding your personal data:</p>
-    <p className="privacy-paragraph">
-    <ul>
-        <li><strong>The Right to Access:</strong>You have the right to request copies of your personal data. A small fee may apply for ​excessive requests.</li>
-        <li><strong>The Right to Rectification:</strong> You have the right to request corrections to inaccurate or incomplete data.
-        The Right to Erasure: You have the right to request deletion of your data under certain conditions (e.g., data is ​no longer needed).</li>
-        <li><strong>The Right to Restrict Processing:</strong> You have the right to request limitations on processing your data under ​specific conditions (e.g., contesting data accuracy).</li>
-        <li><strong>The Right to Object to Processing:</strong> You have the right to object to data processing based on legitimate ​interests or for direct marketing.</li>
-        <li><strong>The Right to Data Portability:</strong> You have the right to request transfer of your data to another organization or ​directly to you.</li>
-        <li><strong>The Right to Withdraw Consent:</strong> You have the right to withdraw consent for data processing at any time.</li>
-        <li><strong>The Right to File a Complaint:</strong> You have the right to file a complaint with the relevant supervisory authority if ​your data rights are violated.</li>
-      </ul>
-    </p>
-      <h2 className="privacy-subheader">Third-Party Services</h2>
-      <p className="privacy-paragraph">We may engage third-party companies and individuals as "Service Providers" to facilitate our services, deliver the services on our behalf, carry out service-related tasks, or aid us in analyzing our service usage.</p>
-      <h2 className="privacy-subheader">Types of Third-Party Services We Use:</h2>
-      <p className="privacy-paragraph">
-    <ul>
-        <li><strong>Payment Processors:</strong>We use third-party payment processors to handle transactions securely. These ​processors adhere to the standards set by PCI-DSS as managed by the PCI Security Standards Council, which ​ensures the secure handling of payment information.</li>
-        <li><strong>Analytics Providers:</strong> We employ third-party analytics tools, such as Google Analytics, to gain insights into the ​usage of our services. These tools collect information sent by your browser as part of a web page request, ​including cookies and your IP address.</li>
-        <li><strong>Marketing and Advertising Partners:</strong> We collaborate with marketing and advertising partners to deliver ​relevant ads and measure their effectiveness. These partners may use cookies and similar tracking ​technologies to collect information about your online activity.</li>
-        <li><strong>Hosting Services:</strong> We rely on third-party hosting services to provide the necessary infrastructure to run our ​website and services securely and efficiently.</li>
+        <li><strong>To Provide Services:</strong> Deliver services, process transactions, and communicate with you about your account.</li>
+        <li><strong>To Improve Experiences:</strong> Optimize website functionality, personalize user interactions, and improve service offerings.</li>
+        <li><strong>To Send Communications:</strong> Share updates, marketing messages, and account notifications based on your preferences.</li>
+        <li><strong>To Ensure Compliance: </strong> Comply with legal obligations and regulatory requirements.</li>
     </ul>
     </p>
-    <h2 className="privacy-subheader">Data Sharing with Third-Party Services:</h2>
-    <p className="privacy-paragraph">
-    <ul>
-        <li><strong>Compliance with Laws:</strong>We may disclose your personal data to third parties if required to do so by law or in ​response to valid requests by public authorities (e.g., a court or a government agency).</li>
-        <li><strong>Business Transfers:</strong> In the event of a merger, acquisition, or sale of all or a portion of our assets, your personal ​data may be transferred to the acquiring organization.</li>
-        <li><strong>Service Improvement:</strong> We may share aggregated, non-identifiable data with our partners and service ​providers to improve our services and provide better user experiences.</li>
-        <li><strong>Data Security with Third-Party Services:</strong> We ensure that any third party we engage with provides adequate ​protection for your personal data. We enter into data processing agreements with third parties to ensure that ​they comply with high standards of data protection and confidentiality.</li>
-        <li><strong>Your Choices:</strong> You have the right to opt out of certain third-party services. For example, you can use browser ​settings to block cookies or use opt-out tools provided by third-party services to manage your preferences.</li>
-        </ul>
-    </p>
-        <h2 className="privacy-subheader">Data Retention</h2>
-        <p className="privacy-paragraph">We only keep your personal data for as long as it's required to accomplish the goals for which we collected it, to ​meet legal requirements, and for backup and archival purposes.</p>
-    <h2 className="privacy-subheader">Retention Periods:</h2>
-    <p className="privacy-paragraph">
-    <ul>
-        <li><strong>Account Information:</strong>Retained while your account is active or as needed for our services.</li>
-        <li><strong>Transaction Data:</strong> Retained for a minimum of seven years for auditing and compliance.</li>
-        <li><strong>Marketing Data:</strong> Retained until you opt out of marketing communications.</li>
-        <li><strong>Usage Data:</strong>Retained for internal analysis and security improvements.</li>
-        </ul>
-    </p>
-    <h2 className="privacy-subheader">Criteria for Determining Retention:</h2>
-    <p className="privacy-paragraph">
-    <ul>
-        <li>Purpose of data collection</li>
-        <li>Nature and sensitivity of data</li>
-        <li>Risk of harm from unauthorized use</li>
-        <li>Legal and regulatory requirements</li>
-        </ul>
-    </p>
-    <p className="privacy-paragraph"> <strong>Deletion and Anonymization:</strong>When no longer needed, data undergoes deletion or anonymization  When ​immediate deletion isn't feasible, we securely store it until it can be deleted.</p>
-    <h2 className="privacy-subheader">Changes to This Privacy Policy</h2>
-    <p className="privacy-paragraph">We may update our Privacy Policy from time to time to reflect changes in our practices, legal requirements, or ​for other operational reasons. When we make changes to this policy, we will notify you by updating the date at ​the top of this document and, if the changes are significant, we may provide a more prominent notice (such as a ​notification on our website or an email notification).</p>
-    <p className="privacy-paragraph">We encourage you to review this Privacy Policy periodically to stay informed about how we are protecting your ​information. Your continued use of our services after any changes to this Privacy Policy constitutes your ​acceptance of such changes.</p>
-    <h2 className="privacy-subheader">Contact Information</h2>
-      <p>If you have any questions, concerns, or requests regarding this Privacy Policy or your personal data, please ​contact us using the information below:</p>
-      <p>Email: <a href="mailto:HR-HQ7@TreasureHint.com" style={{ color: '#f8cf40' }}>HR-HQ7@TreasureHint.com</a></p>
+    <h2 className="privacy-subheader">SMS/MMS Messaging Terms</h2>
+    <p className="privacy-paragraph">We use SMS/MMS messaging to send essential updates, promotional offers, and notifications to users who have opted to receive such messages.</p>
+    
+    <h2 className="privacy-subheader">Consent and Opt-In</h2>
+    <p className="privacy-paragraph">By providing your phone number and explicitly opting in, you consent to receive SMS/MMS messages from Treasure Hint. These may include service updates, marketing communications, and other relevant notifications.</p>
+   
+    <h2 className="privacy-subheader">Message Frequency and Fees</h2>
+    <p className="privacy-paragraph">Message frequency varies based on your engagement. <strong>Standard message and data rates may apply</strong>.</p>
+   
+    <h2 className="privacy-subheader">Opt-Out Instructions</h2>
+    <p className="privacy-paragraph">You may opt out of receiving SMS/MMS messages at any time by replying <strong>"STOP"</strong> to any message. Once you opt out, you will no longer receive SMS/MMS communications unless you opt in again.</p>
+   
+    <h2 className="privacy-subheader">Assistance and Support</h2>
+    <p className="privacy-paragraph">For help with SMS/MMS communications, reply <strong>"TEAM"</strong>or contact us at </p>
+    <p><a href="mailto:HR-HQ7@TreasureHint.com" style={{ color: '#f8cf40' }}>HR-HQ7@TreasureHint.com</a></p>
 
+
+    <h2 className="privacy-subheader">How We Protect Your Information</h2>
+      <p className="privacy-paragraph">We adopt robust data collection, storage, and processing practices to protect your data from unauthorized access, alteration, disclosure, or destruction. Our security measures include encryption, firewalls, and secure data handling protocols.</p>
+     
+      <h2 className="privacy-subheader">Sharing Your Personal Information</h2>
+     
+      <p className="privacy-paragraph">We do not sell, trade, or rent users' data to third parties. However, we may share information:
+      <ul>
+            <li> With <strong>service providers</strong> who assist in delivering our services (e.g., payment processors, analytics tools).
+            </li>
+            <li> For <strong>legal compliance</strong> (e.g., responding to valid requests by public authorities).
+            </li>
+            <li>  During <strong>business transfers</strong> (e.g., in the event of a merger or acquisition).
+            </li>
+         </ul>
+      </p>
+      <h2 className="privacy-subheader">Cookies and Tracking Technologies</h2>
+      <p className="privacy-paragraph">We use cookies to enhance your experience, analyze site usage, and assist marketing efforts. Users can manage cookie preferences through browser settings or opt-out tools.
+      </p>
+
+      <h2 className="privacy-subheader">Your Data Protection Rights</h2>
+      <p className="privacy-paragraph">You have the following rights:</p>
+    
+    <p className="privacy-paragraph">
+    <ul>
+        <li><strong>Access and Rectification:</strong> Request a copy of your data or correct inaccurate information.</li>
+        <li><strong>Erasure and Restriction:</strong> Request deletion or limitation of your data under specific conditions.</li>
+        <li><strong>Objection and Portability:</strong> Object to processing or requesting data transfer to another organization.</li>
+        <li><strong>Withdraw Consent:</strong> Withdraw consent for data processing at any time.</li>
+        <li><strong>File Complaints:</strong> If your data rights are violated, submit concerns to the relevant authorities.
+        </li>
+   </ul>
+    </p>
+
+    <h2 className="privacy-subheader">Data Retention</h2>
+    <p className="privacy-paragraph">We retain personal data only as long as necessary for the purposes for which it was collected, to comply with legal obligations, or for backup purposes.</p>
+    <ul>
+        <li><strong>Account Data:</strong> Retained while your account is active or until you request deletion.</li>
+        <li><strong>Transaction Data:</strong> Retained for seven years for auditing and compliance.</li>
+        <li><strong>Marketing Data: </strong> Retained until you opt out of communications.</li>
+   </ul>
+      <h2 className="privacy-subheader">Changes to This Privacy Policy </h2>
+      <p className="privacy-paragraph">We may update this Privacy Policy periodically to reflect changes in our practices, legal requirements, or operational reasons. Significant changes will be communicated through notifications on our website or email.</p>
+      
+      <h2 className="privacy-subheader">Contact Information</h2>
+      <p>If you have questions or concerns about this Privacy Policy, please get in touch with us:
+      </p>
+      <ul>
+        <li>Email: <a href="mailto:HR-HQ7@TreasureHint.com" style={{ color: '#f8cf40' }}>HR-HQ7@TreasureHint.com</a></li>
+        <li>Phone Number: +1 (886) 677-HINT</li>
+   </ul>
+   <p>We are committed to addressing your inquiries and ensuring your data privacy.
+   </p>
     </div>
     </div>
   );
